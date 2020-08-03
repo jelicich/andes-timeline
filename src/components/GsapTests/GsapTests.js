e0c9421b60ecb6f2a300050ed2ad8a454d33af34
martin.sazzaroli <martin.sazzaroli@mediamonks.com>
2020-08-02 22:41:21 -0300

    Test updates

--- src/components/GsapTests/GsapTests.js
+++ src/components/GsapTests/GsapTests.js
@@ -6,7 +6,7 @@ export default {
   props: [],
   data () {
     return {
-
+      timeline: gsap.timeline()
     }
   },
   computed: {
@@ -16,14 +16,22 @@ export default {
   },
   methods: {
     turnPlane() {
-        gsap.to(this.$refs.plane, {
+
+      if(this.timeline.isActive()){
+        return 
+      } else {
+        this.timeline.to(this.$refs.plane, {
           duration:0.3,  
           scaleX:0, 
           repeat:1, 
           yoyo:true,
-          ease: "power2.inOut"
-        });
-
+          ease: "power2.inOut",
+        })
+        .to(this.$refs.timeline, {
+          duration:1,
+          x:'-=300',
+        }, '-=0.3')
+      }
         this.$refs.plane.classList.toggle('turn-right');
     }
   }
