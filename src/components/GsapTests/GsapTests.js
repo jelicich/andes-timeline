3932fc1b26d871dd879f66e990ed1fbeab228441
martin.sazzaroli <martin.sazzaroli@mediamonks.com>
2020-08-02 22:46:45 -0300

    Update indents

--- src/components/GsapTests/GsapTests.js
+++ src/components/GsapTests/GsapTests.js
@@ -1,38 +1,38 @@
 import gsap from 'gsap'
 
 export default {
-  name: 'gsap-tests',
-  components: {},
-  props: [],
-  data () {
-    return {
-      timeline: gsap.timeline()
+    name: 'gsap-tests',
+    components: {},
+    props: [],
+    data () {
+        return {
+            timeline: gsap.timeline()
+        }
+    },
+    computed: {
+        
+    },
+    mounted () {
+    },
+    methods: {
+        turnPlane() {
+            
+            if(this.timeline.isActive()){
+                return 
+            } else {
+                this.timeline.to(this.$refs.plane, {
+                    duration:0.3,  
+                    scaleX:0, 
+                    repeat:1, 
+                    yoyo:true,
+                    ease: "power2.inOut",
+                })
+                .to(this.$refs.timeline, {
+                    duration:1,
+                    x:'-=300',
+                }, '-=0.3')
+            }
+            this.$refs.plane.classList.toggle('turn-right');
+        }
     }
-  },
-  computed: {
-
-  },
-  mounted () {
-  },
-  methods: {
-    turnPlane() {
-
-      if(this.timeline.isActive()){
-        return 
-      } else {
-        this.timeline.to(this.$refs.plane, {
-          duration:0.3,  
-          scaleX:0, 
-          repeat:1, 
-          yoyo:true,
-          ease: "power2.inOut",
-        })
-        .to(this.$refs.timeline, {
-          duration:1,
-          x:'-=300',
-        }, '-=0.3')
-      }
-        this.$refs.plane.classList.toggle('turn-right');
-    }
-  }
 }
