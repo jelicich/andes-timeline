0d785d7c2e25bb56e10a23e56d105e6df044a064
Esteban Jelicich <ejelicich@web.com>
2020-08-04 00:47:55 -0300

    Move slides on scroll

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,27 +1,39 @@
+import gsap from 'gsap';
+import Util from '../../service/util';
+
+const util = new Util();
+const SLIDER_SEL = '#slider';
+
 export default {
     name: 'slider',
     components: {},
     props: [],
     data() {
         return {
+            tl: gsap.timeline(),
+            // TweenLite.defaultEase = Power2.easeInOut;
+            activeSlide: 0,
+            oldSlide: 0,
+            
+            
             slides: [
                 {
-                    title: 'Pasa algo',
+                    title: '1 Pasa algo',
                     description: 'Se describe algo',
                     date: '02-2006',
                 },
                 {
-                    title: 'Pasa algo mas',
+                    title: '2 Pasa algo mas',
                     description: 'Se describe algo mas',
                     date: '09-2008',
                 },
                 {
-                    title: 'No pasa nada',
+                    title: '3 No pasa nada',
                     description: 'No se describe algo',
                     date: '12-2010',
                 },
                 {
-                    title: 'Se acabó',
+                    title: '4 Se acabó',
                     description: 'Todo, todillo',
                     date: '02-2020',
                 },
@@ -32,9 +44,42 @@ export default {
 
     },
     mounted() {
-
+        console.log('mounter')
+        window.addEventListener('wheel', this.moveSlide);
     },
     methods: {
+        
 
+        moveSlide: function(e) {
+            console.log('yo')
+            // if the container is animating the wheel won't work
+            if (this.tl && this.tl.isActive()) {
+                return;
+            }
+            // temp variable to see if we're at the beginning or end
+            this.oldSlide = this.activeSlide;
+            // which way did we scroll the mousewheel
+            this.activeSlide = e.deltaY > 0 ? (this.activeSlide += 1) : (this.activeSlide -= 1);
+            // are we at the beginning of the slides?
+            this.activeSlide = this.activeSlide < 0 ? 0 : this.activeSlide;
+            // are we at the end of the slides?
+            this.activeSlide = this.activeSlide > this.slides.length - 1 ? this.slides.length - 1 : this.activeSlide;
+            // if at the beginning or end there is nothing to animate
+            if (this.oldSlide === this.activeSlide) {
+                return;
+            }
+            // if not at the beginning or end, we can animate the container 
+            // and the targets to the new position
+            //this.tl = new TimelineMax();
+            //this.tl.to(SLIDER_SEL, 0.4, { xPercent: -100 / slides.length * activeSlide });
+            this.tl.to(SLIDER_SEL, {
+                duration:1,
+                // xPercent: -100 / this.slides.length * this.activeSlide
+                // x: '-=300',
+                x:`${-util.vw(100) * this.activeSlide}`,
+                // transform: 'translateX(-100vw)'
+            })
+
+        }
     },
 };
